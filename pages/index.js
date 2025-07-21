953ea9c10835dd4031bfd3b981c989a119358cb7
Ricardo Leão <113477303+Ricardoleaom@users.noreply.github.com>
2025-07-21 14:34:15 +0000

    Adcionar a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>Nay te amo e vc trouxe para nossas vidas um dos maiores presentes do mundo, uma sobrinha mais linda e fofa do universo. &hearts; </h1>
+}
+
+export default Home;
