0162a4bde9ffc68d692cfd0a847a035a840b372e
Ricardo Leão <113477303+Ricardoleaom@users.noreply.github.com>
2025-07-23 16:05:17 +0000

    estilizando um pouco para testar

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home(){
-    return <h1>Nay te amo e vc trouxe para nossas vidas um dos maiores presentes do mundo, uma sobrinha mais linda e fofa do universo.💓 </h1>
-}
+    return (
+        <div className='container'>
+            <h1>Nay te amo e vc trouxe para nossas vidas um dos maiores presentes do mundo, uma sobrinha mais linda e fofa do universo.💓 </h1>
+            <button onClick={() => window.open('/img/sorriso.jpg', '_blank')}>
+                Sorria Comigo
+                </button>
+        </div>
+        ); 
+    }
 
 export default Home;
