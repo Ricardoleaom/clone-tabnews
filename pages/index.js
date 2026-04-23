edbb6e94ad049a3ff40b6162008b0870acabd743
Ricardo Leao <ricardoleaom@gmail.com>
2026-04-23 13:03:21 -0300

    adicioando mensagem Em Construção

--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>Leticia, minha rainha! Eu te amo muito!</h1>
-      <h1>I Don't Want to Miss a Thing!</h1>
-      <p>I could stay awake just to hear you breathing</p>
-      <p>Watch you smile while you are sleeping</p>
-      <p>While you're far away and dreaming</p>
-      <p>I could spend my life in this sweet surrender</p>
-      <p>I could stay lost in this moment forever</p>
-      <p>Where a moment spent with you is a moment I treasure</p>
-      <p>Don't wanna close my eyes</p>
-      <p>I don't wanna to fall asleep</p>
-      <p>'Cause I'd miss you baby</p>
-      <p>And I don't wanna miss a thing</p>
-      <p>'Cause even when I dream of you (even when I dream)</p>
-      <p>The sweetest dream would never do</p>
-      <p>I'd still miss you baby</p>
-      <p>And I don't wanna miss a thing</p>
-      <p>Lying close to you, feeling your heart beating</p>
-      <p>And I'm wondering what you're dreaming</p>
-      <p>Wondering if it's me you're seeing</p>
-      <p>Then I kiss your eyes</p>
-      <p>And thank God we're together</p>
-      <p>And I just wanna stay with you in this moment forever</p>
-      <p>Forever and ever</p>
-      <p>I don't wanna close my eyes</p>
-      <p>I don't wanna to fall asleep</p>
-      <p>'Cause I'd miss you baby</p>
-      <p>And I don't wanna miss a thing</p>
-      <p>'Cause even when I dream of you (even when I dream)</p>
-      <p>The sweetest dream will never do</p>
-      <p>I'd still miss you baby</p>
-      <p>And I don't wanna miss a thing</p>
-      <p>I don't wanna miss one smile</p>
-      <p>And I don't wanna miss one kiss</p>
-      <p>And I just wanna be with you</p>
-      <p>Right here with you, just like this</p>
-      <p>And I just wanna hold you close</p>
-      <p>I feel your heart so close to mine</p>
-      <p>And just stay here in this moment</p>
-      <p>For all the rest of time</p>
-      <p>Yeah, yeah, yeah, yeah, yeah</p>
-      <p>Don't wanna close my eyes</p>
-      <p>Don't wanna fall asleep</p>
-      <p>'Cause I'd miss you baby</p>
-      <p>And I don't wanna miss a thing</p>
-      <p>'Cause even when I dream of you (even when I dream)</p>
-      <p>The sweetest dream would never do</p>
-      <p>I'd still miss you baby</p>
-      <p>And I don't wanna miss a thing</p>
-      <p>Don't wanna close my eyes</p>
-      <p>I don't wanna fall asleep, yeah</p>
-      <p>And I don't wanna miss a thing</p>
-      <p>I love you, Leticia</p>
-      <a href="https://music.apple.com/br/album/i-dont-want-to-miss-a-thing-from-armageddon-soundtrack/1704953784?i=1704954398">
-        I Don't Want to Miss a Thing
-      </a>
+      <h1>Em Construção</h1>
     </div>
   );
 }
